01ec9b736f636c2c7434fcb5109eed0d29a2a701
Michael Boateng <112430340+michaelboat@users.noreply.github.com>
2025-08-18 11:08:59 -0400

    applied migrations

--- network/models.py
+++ network/models.py
@@ -4,3 +4,12 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Post(models.Model):
+    content = models.CharField(max_length=140)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user")
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Post {self.id} written by {self.author} on {self.date.strftime('%d %b %Y %H:%M:%S')}"
+
